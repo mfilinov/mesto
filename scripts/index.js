300beb3f162d2cb0faf7760a856190b6ae1ce26a
Maksim Filinov <mfilinov@yandex.ru>
2023-03-09 21:51:24 +0300

    5 sprint init

--- scripts/index.js
+++ scripts/index.js
@@ -5,27 +5,75 @@ let nameInput = formElement.querySelector('.popup__input_name_text');
 let jobInput = formElement.querySelector('.popup__input_job_text');
 let popupButtonClose = popup.querySelector('.popup__button-close');
 
-//Объекты из секции profile
+//Section profile
 const profileBlock = document.querySelector('.profile__text');
 const buttonEdit = profileBlock.querySelector('.profile__button-edit');
 let profileTitle = profileBlock.querySelector('.profile__title');
 let profileSubtitle = profileBlock.querySelector('.profile__subtitle');
 
+//Section elements
+const elementsList = document.querySelector('.elements__list');
 
-function openPopup() {
+//Templates
+const elementTemplate = document.querySelector('#element-template').content;
+
+
+const createElement = (name, link) => {
+  const element = elementTemplate.querySelector('.element').cloneNode(true);
+  element.querySelector('.element__image').src = link;
+  element.querySelector('.element__image').alt = `Изображение ${name}`;
+  element.querySelector('.element__title').textContent = name;
+  //Изменение состояния like после клика
+  element.querySelector('.element__like').addEventListener('click', (evt) => {
+    evt.target.classList.toggle('element__button-like_active');
+  })
+  elementsList.append(element);
+}
+
+const initialCards = [
+  {
+    name: 'Пятигорск',
+    link: './images/pyatigorsk.jpeg'
+  },
+  {
+    name: 'Кисловодск',
+    link: './images/kislovodsk.jpeg'
+  },
+  {
+    name: 'Великий Новгород',
+    link: './images/novgorod.jpeg'
+  },
+  {
+    name: 'Махачкала',
+    link: './images/makhachkala.jpeg'
+  },
+  {
+    name: 'Владикавказ',
+    link: './images/vlidikavkaz.jpeg'
+  },
+  {
+    name: 'Грозный',
+    link: './images/grozniy.jpeg'
+  }
+];
+
+initialCards.forEach(function (item) {
+  createElement(item.name, item.link);
+})
+
+const openPopup = () => {
   popup.classList.add('popup_opened');
   // При открытии формы поля «Имя» и «О себе» должны быть заполнены теми значениями, которые отображаются на странице.
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
 }
 
-function closePopup() {
+const closePopup = () => {
   popup.classList.remove('popup_opened');
 }
 
-function handleFormSubmit(evt) {
+const handleFormSubmit = (evt) => {
   evt.preventDefault();
-
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
   closePopup();
